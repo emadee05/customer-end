6b34ab8f2018b011f7aa9082a791607a49a2dd12
emadee05 <emadee.017@gmail.com>
2025-07-07 10:51:31 -0700

    initial

---
+++ hireez-portal/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="button">
+          <div>Upload</div>
+          <small>Upload your resume to find matching jobs</small>
+        </div>
+        <div className="button">
+          <div>Connect</div>
+          <small>Draft and send cold emails to recruiters</small>
+        </div>
+        <div className="button">
+          <div>Manage</div>
+          <small>View the progress of your applications</small>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
